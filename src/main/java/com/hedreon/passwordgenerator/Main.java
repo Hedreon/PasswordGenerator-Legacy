586f851b9266c33cf74e2b4896e3f9987208e42c
Hedreon <39467046+Hedreon@users.noreply.github.com>
2022-08-31 20:42:25 +0100

    Add theme synchronization

--- src/main/java/com/hedreon/passwordgenerator/Main.java
+++ src/main/java/com/hedreon/passwordgenerator/Main.java
@@ -2,13 +2,22 @@ package com.hedreon.passwordgenerator;
 
 // Imports
 import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.jthemedetecor.OsThemeDetector;
 
 import java.awt.EventQueue;
 
 public class Main {
     public static void main(String[] args) {
         // Setup look and feel
-        FlatDarkLaf.setup();
+        final OsThemeDetector themeDetector = OsThemeDetector.getDetector();
+        final boolean usesDarkTheme = themeDetector.isDark();
+
+        if (usesDarkTheme) {
+            FlatDarkLaf.setup();
+        } else {
+            FlatLightLaf.setup();
+        }
 
         // Launch app
         EventQueue.invokeLater(() -> {
